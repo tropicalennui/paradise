04018c062030091c059a1eb35b877e6e2811c8cd
tropicalennui <51432174+tropicalennui@users.noreply.github.com>
2022-11-08 19:19:43 +1000

    font preview page done for now

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
+const fonts = ['Caveat Brush', 'Berkshire Swash', 'Sansita', 'Aclonica', 'Norican', 'Rancho', 'Pattaya', 'Oleo Script', 'Lobster', 'Pacifico', 'Acme', 'Lobster Two', 'Sacramento'];
+createGrid(4, 3);
+
 function createGrid(num1, num2) {
-	numDivs = num1 * num2;
-	const container = document.querySelector('#container');
+	const numDivs = num1 * num2;
+    const container = document.getElementById('container');
+    let count = 0;
 
 	for (x = 0; x < numDivs; x++) {
-		const grid = document.createElement('div');
-		grid.classList.add('box');
+        let fontClass = 'font' + (x+1);
+        const grid = document.createElement('div');
+        grid.classList.add('cell');
+        grid.classList.add(fontClass);
 		container.appendChild(grid);
 	}
 
-    let boxes = document.querySelectorAll('.box')
+    let boxes = document.querySelectorAll('.cell')
     boxes.forEach(box => {
-        box.innerText = 'Paradise Woodwork'
+        box.innerText = 'Paradise Woodwork';
+        const fontName = document.createElement('div');
+        fontName.classList.add('plain');
+        fontName.innerText = fonts[count];
+        box.appendChild(fontName);
+        count++;
     })
 
 
 
 }
-
-createGrid(4, 3);
