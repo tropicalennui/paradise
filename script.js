834d82845a7875dd54924b7f122dd8182df1abdc
tropicalennui <51432174+tropicalennui@users.noreply.github.com>
2022-11-08 18:28:09 +1000

    added inner text

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function createGrid(num) {
+function createGrid(num1, num2) {
 	numDivs = num * num;
-	const container = document.querySelector('.container');
+	const container = document.querySelector('#container');
 
 	for (x = 0; x < numDivs; x++) {
 		const box = document.createElement('div');
@@ -8,6 +8,13 @@ function createGrid(num) {
 		container.appendChild(box);
 	}
 
+    let boxes = document.querySelectorAll('.box')
+    boxes.forEach(box => {
+        box.innerText = 'Paradise Woodwork'
+    })
+
+
+
 }
 
-createGrid(4);
+createGrid(4, 3);
